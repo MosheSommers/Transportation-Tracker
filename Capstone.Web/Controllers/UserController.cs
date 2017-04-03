51f0ed6b6bfd0cfe11f772e0d43fdd2852143783
Moshe SOmmers <moshesommers@gmail.com>
2017-04-03 12:14:27 -0400

    Created basic MVC layout

---
+++ Capstone.Web/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
