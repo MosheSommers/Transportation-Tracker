c0d7441cab858725f3cfe28bf5890a92257c7599
Moshe SOmmers <moshesommers@gmail.com>
2017-04-03 14:30:26 -0400

    set up UserDal with interface and configuration, created getUser method

---
+++ Capstone.Web/DAL/UserSqlDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Capstone.Web.Models;
+
+namespace Capstone.Web.DAL
+{
+    public class UserSqlDAL : IUserDAL
+    {
+        public User GetUser(User u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertNewUser(User u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Capstone.Web/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string EmailAddress { get; set; }
+        public string Phone { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
