142097b9b21d91f8963fa13c5e69a14d452c5c5f
Antoni Chopcinski <a.chopcinski@gmail.com>
2017-04-03 13:59:48 -0400

    Schema for users created

--- database/schema.sql
+++ database/schema.sql
@@ -2,8 +2,17 @@
 -- This script creates all of the database objects (tables, constraints, etc) for the database
 -- *************************************************************************************************
 
-BEGIN;
+
+
 
 -- CREATE statements go here
+CREATE table users
+(
+	user_id int Identity(1,1),
+	email_address varChar(150) not null,
+	password varChar(50) not null,
+	phone_number varChar(10),
+	is_admin bit
 
-COMMIT;
+	CONSTRAINT pk_users PRIMARY KEY (user_id)
+);
