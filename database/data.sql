7ed0386b712748555c72f3d77d8e50b318020152
Antoni Chopcinski <a.chopcinski@gmail.com>
2017-04-03 15:25:01 -0400

    Database insert statements for sql created

--- database/data.sql
+++ database/data.sql
@@ -5,5 +5,16 @@
 BEGIN;
 
 -- INSERT statements go here
+INSERT INTO [dbo].[users]
+           ([email_address]
+           ,[password]
+           ,[phone_number]
+           ,[is_admin])
+     VALUES
+           (<email_address, varchar(150),>
+           ,<password, varchar(50),>
+           ,<phone_number, varchar(10),>
+           ,<is_admin, bit,>)
+GO
 
 COMMIT;
