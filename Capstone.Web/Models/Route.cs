98aced36754ca896522b0a393bf47b4527df9437
Nathan Rathbun <naterathbun@gmail.com>
2017-04-04 10:00:31 -0400

    Added Route Model

---
+++ Capstone.Web/Models/Route.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Route
+    {
+        public string RouteName { get; set; }
+        public int RouteID { get; set; }
+        public int PrivateNumber { get; set; }
+        public string CreatedBy { get; set; }
+        public bool IsPrivate { get; set; }
+
+        public List<string> Waypoints { get; set; } = new List<string>();
+
+        public void AddWayPoint(string newWaypoint)
+        {
+            this.Waypoints.Add(newWaypoint);
+        }
+    }
+}
--- Capstone.Web/Models/User.cs
+++ Capstone.Web/Models/User.cs
@@ -12,5 +12,6 @@ namespace Capstone.Web.Models
         public string Phone { get; set; }
         public bool IsAdmin { get; set; }
         public string Password { get; set; }
+        public string Salt { get; set; }
     }
 }
