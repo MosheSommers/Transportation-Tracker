036ebd69979c4f83a28048244071d03e716aecf9
Antoni Chopcinski <a.chopcinski@gmail.com>
2017-04-04 12:15:56 -0400

    updated schema for normalisation and data insert to reflect changes in schema

--- database/data.sql
+++ database/data.sql
@@ -5,16 +5,45 @@
 BEGIN;
 
 -- INSERT statements go here
+USE [omnibus]
+GO
+
+INSERT INTO [dbo].[routes]
+           ([route_name])
+     VALUES
+           (<route_name, varchar(100),>)
+GO
+
 INSERT INTO [dbo].[users]
            ([email_address]
            ,[password]
+           ,[salt]
            ,[phone_number]
            ,[is_admin])
      VALUES
            (<email_address, varchar(150),>
            ,<password, varchar(50),>
+           ,<salt, varchar(10),>
            ,<phone_number, varchar(10),>
            ,<is_admin, bit,>)
 GO
+
+USE [omnibus]
+
+GO
+
+INSERT INTO [dbo].[waypoints]
+           ([waypoint_position]
+           ,[waypoint_name]
+           ,[stop_number]
+           ,[route_id])
+     VALUES
+           (<waypoint_position, varchar(200),>
+           ,<waypoint_name, varchar(50),>
+           ,<stop_number, int,>
+           ,<route_id, int,>)
+GO
+
+
 
 COMMIT;
--- database/schema.sql
+++ database/schema.sql
@@ -2,8 +2,11 @@
 -- This script creates all of the database objects (tables, constraints, etc) for the database
 -- *************************************************************************************************
 
+DROP TABLE users;
+DROP TABLE routes;
+DROP TABLE waypoints;
 
-
+--DROP TABLE private_group;
 
 -- CREATE statements go here
 CREATE table users
@@ -11,19 +14,40 @@ CREATE table users
 	user_id int Identity(1,1),
 	email_address varChar(150) not null,
 	password varChar(50) not null,
+	salt varChar(10) not null,
 	phone_number varChar(10),
 	is_admin bit
 
-	CONSTRAINT pk_users PRIMARY KEY (user_id)
+	CONSTRAINT PK_users PRIMARY KEY (user_id)
 );
 
 CREATE table routes
 (
 	route_id int Identity(1,1),
 	route_name varChar(100) not null,
-	waypoints varChar(max) not null,
-	created_by_user int
 
-	CONSTRAINT pk_routes PRIMARY KEY (route_id)
+	CONSTRAINT PK_routes PRIMARY KEY (route_id)
 );
+
+CREATE table waypoints
+(
+	waypoint_id int Identity(1,1),
+	waypoint_position varChar(200),
+	waypoint_name varChar(50),
+	stop_number int, 
+	route_id int FOREIGN KEY REFERENCES routes(route_id),
+	CONSTRAINT PK_waypoint_id PRIMARY KEY (waypoint_id)	
+);
+
+--CREATE table private_group
+--(
+--	private_group_id int Identity(1,1),
+--	private_group_name varChar(50) not null,
+--	user_id int FOREIGN KEY REFERENCES users(user_id)
+
+--	CONSTRAINT PK_private_group PRIMARY KEY (private_group_id)
+--);
+
+
+
 
