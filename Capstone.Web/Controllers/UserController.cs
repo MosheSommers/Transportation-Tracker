a42cac456b188265657650d7005dae2410f0cdd4
Moshe SOmmers <moshesommers@gmail.com>
2017-04-04 17:15:22 -0400

    Completed userController and login view

--- Capstone.Web/Controllers/UserController.cs
+++ Capstone.Web/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Capstone.Web.DAL;
+using Capstone.Web.Models;
 
 namespace Capstone.Web.Controllers
 {
@@ -16,9 +17,26 @@ namespace Capstone.Web.Controllers
             this.userDAL = userDAL;
         }
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginPost(String email, String password)
+        {
+            User u = new User() { EmailAddress = email, Password = password };
+            User validatedUser = userDAL.GetUser(u);
+            if (validatedUser.EmailAddress != null && validatedUser.Password == u.Password)
+            {
+                Session["Login"] = validatedUser;
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         //Get: Register
