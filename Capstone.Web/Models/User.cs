a9f9b9d206529ce5d1ca41a12e94dea05ab48a3e
Rachel Funk <rachelleahlong@gmail.com>
2017-04-07 11:53:28 -0400

    started adding verifications to user model for login

--- Capstone.Web/Models/User.cs
+++ Capstone.Web/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,20 @@ namespace Capstone.Web.Models
     public class User
     {
         public int UserID { get; set; }
+
+        [Required(ErrorMessage = "An email address is required to register")]
         public string EmailAddress { get; set; }
+
+
         public string Phone { get; set; }
         public bool IsAdmin { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+
         public string Salt { get; set; }
+
+
     }
 }
