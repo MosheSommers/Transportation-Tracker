763f5e8f8bceb8c8e131e8b44b0bb5fb60858fe4
Nathan Rathbun <naterathbun@gmail.com>
2017-04-11 09:45:45 -0400

    Adding functionality for Waypoint Times in Route views, models, controllers, DALs

--- Capstone.Web/Models/Route.cs
+++ Capstone.Web/Models/Route.cs
@@ -7,7 +7,7 @@ namespace Capstone.Web.Models
 {
     public class Route
     {
-        
+
         private string routeName;
 
         public string RouteName
@@ -22,10 +22,33 @@ namespace Capstone.Web.Models
         public bool IsPrivate { get; set; }
 
         public List<string> Waypoints { get; set; } = new List<string>();
+        public List<string> Times { get; set; } = new List<string>();
+        public List<string> Users { get; set; } = new List<string>();
 
         public void AddWaypoint(string newWaypoint)
         {
             this.Waypoints.Add(newWaypoint);
+        }
+
+        public void AddTime(string newTime)
+        {
+            this.Times.Add(newTime);
+        }
+
+        public void AddUser(string User)
+        {
+            if (!this.Users.Contains(User))
+            {
+                this.Users.Add(User);
+            }
+        }
+
+        public void RemoveUser(string User)
+        {
+            if (this.Users.Contains(User))
+            {
+                this.Users.Remove(User);
+            }
         }
     }
 }
