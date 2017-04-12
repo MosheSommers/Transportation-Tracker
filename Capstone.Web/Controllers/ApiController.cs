7a9d7f31195a8c55489479fd2a5511ad078bc46b
Nathan Rathbun <naterathbun@gmail.com>
2017-04-12 13:19:04 -0400

    Added basic API and websocket

---
+++ Capstone.Web/Controllers/ApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class ApiController : Controller
+    {
+        // POST: Api
+        [HttpPost]
+        public ActionResult UpdateLocation(double longitude, double latitude, int routeId)
+        {
+            // Notify connected clients about update to bus location
+            var websocketContext = GlobalHost.ConnectionManager.GetConnectionContext<TransportationConnection>();
+
+            var update = new
+            {
+                routeId = routeId,
+                latitude = latitude,
+                longitude = longitude
+            };
+
+            websocketContext.Connection.Broadcast(update);
+
+            return Json(true);
+        }
+    }
+}
---
+++ Capstone.Web/TransportationConnection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web
+{
+    public class TransportationConnection : PersistentConnection
+    {
+
+    }
+}
